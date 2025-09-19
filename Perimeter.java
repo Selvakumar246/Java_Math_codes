4e3183a38d922a8fe07bcadf36786d3c5caca3d3
Selvakumar246 <vjselvakumarofficial@gmail.com>
2025-09-19 22:17:50 +0530

    created Perimeter.java

--- Perimeter.java
+++ Perimeter.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
 
+public class Perimeter {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+       System.out.println("enter the radius of the circle:");
+       double rad=input.nextInt();
+       double Per=2* (Math.PI)*rad;
+       System.out.printf("the Perimeter of the Circle is:%.2f units",Per);
+
+
+       System.out.println("enter the side of an triangle");
+       double side = input.nextInt();
+       double Perimeter = 3 * side;
+       System.out.printf("the Perimeter of the Equilateral triangle is:%.2f cm", Perimeter);
+    }
+}
